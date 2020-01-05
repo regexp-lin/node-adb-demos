dc4ff7b203ae713a639b3f8c4e4066e71cabccf9
teddylin <teddylin@tencent.com>
2020-01-06 00:48:38 +0800

    -x

--- demo1/qutoutiao.js
+++ demo1/qutoutiao.js
@@ -7,11 +7,16 @@ setInterval(() => {
     utils.swipeUp()
     shell.exec('adb shell input tap 900 600')
     shell.exec('adb shell input keyevent 4')
+    shell.exec('adb shell input tap 540 2200')
 
 }, 10 * 1000)
 
 // shell.exec('adb shell wm size')
 // shell.exec('adb shell input tap 900 600')
 // shell.exec('adb shell input tap 540 1800')
+// utils.swipeUp()
+// shell.exec('adb shell input tap 900 600')
+// shell.exec('adb shell input keyevent 4')
+// shell.exec('adb shell input tap 540 2200')
 
 
